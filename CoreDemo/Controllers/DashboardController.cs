965ab7318269ee3c90e5dbb56ebfec7f477f1530
erdemaltu <e-altug@hotmail.com>
2022-10-04 17:37:47 +0300

    Add project files.

---
+++ CoreDemo/Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            Context c = new Context();
+            ViewBag.v1 = c.Blogs.Count().ToString();
+            ViewBag.v2 = c.Blogs.Where(x=>x.WriterID==2).Count();
+            ViewBag.v3 = c.Categories.Count();
+            return View();
+        }
+    }
+}
