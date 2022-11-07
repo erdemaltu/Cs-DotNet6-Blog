cf46b440e09f96d478e33ac5e430ddf7722c5a78
erdemaltu <e-altug@hotmail.com>
2022-11-07 17:56:56 +0300

    identity

---
+++ CoreDemo/Models/UserSignInViewModel.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreDemo.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage="Lütfen kullanıcı adını girin")]
+        public string username { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifre girin")]
+        public string password { get; set; }
+    }
+}
--- CoreDemo/Program.cs
+++ CoreDemo/Program.cs
@@ -1,9 +1,18 @@
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<Context>();
+builder.Services.AddIdentity<AppUser, AppRole>(x => 
+{
+    x.Password.RequireUppercase = false;
+    x.Password.RequireNonAlphanumeric = false;
+})
+    .AddEntityFrameworkStores<Context>();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
